1dbb5434fe7b2e64f044a45fea1eaab0963cb0db
unknown <mahtok98212@gmail.com>
2024-07-17 17:17:37 +0530

    day 4 of js

---
+++ day4.js
@@ -0,0 +1,68 @@
+// task 1
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// //task 2
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`5 X ${i} = ${5 * i}`);
+// }
+
+//task 3
+// let counter = 0,
+//   sum = 0;
+// while (counter <= 10) {
+//   sum += counter;
+//   counter++;
+// }
+// console.log(sum);
+
+// // task4
+// let counter = 10;
+// while (counter > 0) {
+//   console.log(counter);
+//   counter--;
+// }
+
+// task 5
+// let counter = 1;
+// do {
+//   console.log(counter);
+//   counter++;
+// } while (counter <= 5);
+
+//task 6
+// let num = 5,
+//   res = 1;
+// do {
+//   res *= num;
+//   num--;
+// } while (num > 0);
+// console.log(res);
+
+// task 7
+// function printStar(rows) {
+//   for (let i = 1; i <= rows; i++) {
+//     for (let j = 1; j <= i; j++) {
+//       process.stdout.write(" * ");
+//     }
+//     console.log();
+//   }
+// }
+// printStar(5);
+
+// task 8
+// for (let i = 1; i <= 10; i++) {
+//   if (i === 5) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// task 9
+// for (let i = 1; i <= 10; i++) {
+//   if (i === 7) {
+//     break;
+//   }
+//   console.log(i);
+// }
