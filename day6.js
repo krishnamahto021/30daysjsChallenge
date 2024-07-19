b8502902915c432d0a734a1880b7f956b91916ce
unknown <mahtok98212@gmail.com>
2024-07-19 14:27:23 +0530

    day 6

---
+++ day6.js
@@ -0,0 +1,55 @@
+// task 1
+// const arr = [1, 2, 3, 4, 5];
+// console.log(arr);
+
+// task 2
+// const arr = [1, 2, 3, 4, 5];
+// console.log(arr[0], arr[arr.length - 1]);
+
+// task 3
+const arr = [1, 2, 3, 4, 5];
+// arr.push(6);
+// console.log(arr);
+
+// task 4
+// arr.pop();
+// console.log(arr);
+
+// task 5
+// arr.shift();
+// console.log(arr);
+
+// task 6
+// arr.unshift(0)
+// console.log(arr);
+
+// task 7
+// const doubledArr = arr.map((ele) => 2 * ele);
+// console.log(doubledArr);
+
+// task 8
+// const evenArr = arr.filter((ele) => ele % 2 === 0);
+// console.log(evenArr);
+
+//task 9
+// const sum = arr.reduce((sum, ele) => sum + ele);
+// console.log(sum);
+
+// // task 10
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// task 11
+// arr.forEach((ele)=>{
+//     console.log(ele);
+// })
+
+// task 12
+const twoDArr = [
+  [1, 2, 3],
+  [4, 5, 6],
+];
+
+// console.log(twoDArr);
+// console.log(twoDArr[0][1]);
