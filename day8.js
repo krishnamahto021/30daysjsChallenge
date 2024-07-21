113295c14a56ba013351d41121dd5c31c38df34f
unknown <mahtok98212@gmail.com>
2024-07-21 10:00:13 +0530

    Day 8 of js challenge

---
+++ day8.js
@@ -0,0 +1,65 @@
+// task 1
+// const name = "Johny vaiya";
+// const age = 69;
+// console.log(`Hey I am ${name} of age ${age}`);
+
+// task 2
+// const multiLineString = "hello i am \nmultiline string \n";
+// console.log(`${multiLineString}`);
+
+// task 3
+// const arr = [1,2,3,4,5];
+// const [first,second]=arr;
+// console.log(first,second);
+
+// task 4
+// const book = {
+//   title: "No rules rules",
+//   author: "Reed hastings",
+//   year: "2020",
+// };
+// const { title, author } = book;
+// console.log(title, author);
+
+// task 5
+// const oldArr = [1,2,3,4,5];
+// const newArr = [...oldArr,6,7,8];
+// console.log(newArr);
+
+// task 6
+// function sum(...nums) {
+//   return nums.reduce((sum, current) => sum + current, 0);
+// }
+// console.log(sum(1,2,3,4));
+// console.log(sum(1, 2, 3, 4,5,6,7));
+// console.log(sum(1, 2, 3));
+
+// task 7
+// function product(a, b = 1) {
+//   console.log(a * b);
+// }
+// product(4);
+// product(4, 5);
+
+// task 8
+// const name = "johny vaiya";
+// const prop = "coactor";
+// const value = "danny ";
+// const object = {
+//   name,
+//   [prop]: value,
+//   greet() {
+//     console.log(`I am ${this.name} and love to work with ${this.coactor}`);
+//   },
+// };
+// console.log(object);
+// object.greet();
+
+// task 9
+// const prop1 = "name";
+// const prop2 = "age";
+// const person = {
+//   [prop1]: "Johny vaiya",
+//   [prop2]: "69",
+// };
+// console.log(person.name, person.age);
